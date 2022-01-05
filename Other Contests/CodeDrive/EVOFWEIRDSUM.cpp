21487a518565d64bcca28f133be5c2465d3db278
parveshbarak <pbarak6@gmail.com>
2022-01-05 12:18:51 +0530

    CodeDrive

--- Other Contests/CodeDrive/EVOFWEIRDSUM.cpp
+++ Other Contests/CodeDrive/EVOFWEIRDSUM.cpp
@@ -1,60 +1,29 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define ll long long int
-#define ld	long double
-#define mod 1000000007
-#define inf 1e18
-#define pb emplace_back
-#define vi vector<ll>
-#define vs	vector<string>
-#define pii pair<ll,ll>
-#define vp vector<pair<ll,ll>>
-#define mp  make_pair
-#define pq_max priority_queue<ll>
-#define pq_min priority_queue<ll,vi,greater<ll> >
-#define ff first
-#define ss second
-#define mid(l,r) (l+(r-l)/2)
-#define loop(i,a,b) for(int i=(a);i<(b);i++)
-#define revloop(i,a,b) for(int i=(a);i>=(b);i--)
-#define printarr(arr,a,b) for(int z=(a);z<=(b);z++) cout<<(arr[z])<<" ";cout<<endl;
-
-
-
-void io_file() {
-    ios_base::sync_with_stdio(0); 
-    cin.tie(0); 
-    cout.tie(0);
-    #ifndef ONLINE_JUDGE
-        freopen("/home/parvesh/Documents/Practice/io_files/input.txt","r", stdin);
-        freopen("/home/parvesh/Documents/Practice/io_files/output.txt","w", stdout);
-        freopen("/home/parvesh/Documents/Practice/io_files/error.txt","w", stderr);
-    #endif
-}
-
-
-void code_here() {
-    ll t; cin >> t;
-    while(t--) {
-        ll n; cin >> n;
-        vi arr(n);
-        loop(i,0,n) cin >> arr[i];
-        ll sum = 0;
-        loop(i,0,n) sum += arr[i];
-        cout << sum/n << "\n";
+class Solution {
+public:
+    int ans = 0;
+    void back(vector<string>& words, vector<int>& score, vector<int> &freq, vector<int> &vis, int pos, int sc) {
+        for (int i = 0; i < 26; ++i) {
+            if (vis[i] > freq[i]) return;
+        }
+        ans = max(sc, ans);
+        for (int i = pos; i < words.size(); ++i) {
+            for (auto & c : words[i]) {
+                vis[c-'a']++;
+                sc += score[c-'a'];
+            }
+            back(words, score, freq, vis, i+1, sc);
+            for (auto & c : words[i]) {
+                vis[c-'a']--;
+                sc -= score[c-'a'];
+            }
+        }
     }
-}
-
-
-int main() {
-    
-    clock_t start = clock();
-    
-    io_file();
-    code_here();
-    
-    clock_t end = clock();
-    cerr<<endl<<endl<<"Executed In: "<<double(end - start) / CLOCKS_PER_SEC*1000<<" ms";
-    
-    return 0;
-}
+    int maxScoreWords(vector<string>& words, vector<char>& letters, vector<int>& score) {
+        vector<int> freq(26, 0), vis(26, 0);
+        for (auto & c : letters) {
+            freq[c-'a']++;
+        }
+        back(words, score, freq, vis, 0, 0);
+        return ans;
+    }
+};
