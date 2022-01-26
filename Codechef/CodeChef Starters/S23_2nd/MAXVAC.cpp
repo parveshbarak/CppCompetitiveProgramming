258dadfe402765051b478a29d3ab52117fb6833c
parveshbarak <pbarak6@gmail.com>
2022-01-26 22:03:19 +0530

    Starters

---
+++ Codechef/CodeChef Starters/S23_2nd/MAXVAC.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ld	long double
+#define mod 1000000007
+#define inf 1e18
+#define pb emplace_back
+#define vi vector<ll>
+#define vs	vector<string>
+#define pii pair<ll,ll>
+#define vp vector<pair<ll,ll>>
+#define mp  make_pair
+#define pq_max priority_queue<ll>
+#define pq_min priority_queue<ll,vi,greater<ll> >
+#define ff first
+#define ss second
+#define mid(l,r) (l+(r-l)/2)
+#define loop(i,a,b) for(int i=(a);i<(b);i++)
+#define revloop(i,a,b) for(int i=(a);i>=(b);i--)
+#define printarr(arr,a,b) for(int z=(a);z<=(b);z++) cout<<(arr[z])<<" ";cout<<endl;
+
+
+
+void io_file() {
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); 
+    cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("/home/parvesh/Documents/Practice/io_files/input.txt","r", stdin);
+        freopen("/home/parvesh/Documents/Practice/io_files/output.txt","w", stdout);
+        freopen("/home/parvesh/Documents/Practice/io_files/error.txt","w", stderr);
+    #endif
+}
+
+
+void code_here() {
+    ll t; cin >> t;
+    while(t--) {
+        ll n,k; cin >> n >> k;
+        string s; cin >> s;
+        if(n==1) cout << "1\n";
+        else {
+            ll ans = 0, i = 0; bool flag = true;
+            while(i<n) {
+                ll zeroes=0;
+                // cout << zeroes << " " << i << " ";
+                for(ll j=i;j<min(n,i+k);j++) {
+                    if(s[j]=='0') zeroes++;
+                }
+                if(zeroes==k) i = i+k, ans++;
+                else if(zeroes==k-1 && flag) flag = false, i = i+k, ans++;
+                else i++;
+                // cout << zeroes << " " << i << "\n";
+            }
+            cout << ans << "\n";
+        }
+    }
+}
+
+
+int main() {
+    
+    clock_t start = clock();
+    
+    io_file();
+    code_here();
+    
+    clock_t end = clock();
+    cerr<<endl<<endl<<"Executed In: "<<double(end - start) / CLOCKS_PER_SEC*1000<<" ms";
+    
+    return 0;
+}
